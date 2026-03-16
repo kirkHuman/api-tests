e615e8dcca9b3289accd9096e8a396ac6d11ccc1
Vladimir <45611716+kirkHuman@users.noreply.github.com>
2026-03-16 14:52:21 -0700

    added tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,8 +7,20 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 2 : undefined,
-  reporter: [['html', { open: 'never' }], ['list']],
+  workers: process.env.CI ? 4 : undefined,
+  reporter: [
+    ['list'],
+    [
+      'playwright-smart-reporter',
+      {
+        outputFile: '../smart-report/index.html',
+        historyFile: '../smart-report/history.json',
+        maxHistoryRuns: 20,
+        projectName: 'api-tests',
+        runId: process.env.GITHUB_RUN_ID,
+      },
+    ],
+  ],
   use: {
     baseURL: BASE_URL,
     extraHTTPHeaders: {
